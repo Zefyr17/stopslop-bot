6b42095c5db8ef23834afdc8f11811e03d504720
@Zefyr17 <wisemole51@gmail.com>
2026-02-11 19:27:25 +0500

    change

--- prisma/migrations/20260211_spam_penalty_per_post/migration.sql
+++ prisma/migrations/20260211_spam_penalty_per_post/migration.sql
@@ -1,9 +1,12 @@
+-- Clear old penalty records (they have no postId and can't be migrated)
+DELETE FROM "SpamPenalty";
+
 -- Drop old unique index and penaltyCount column
 DROP INDEX "SpamPenalty_oderId_guildId_weekStartDate_key";
 ALTER TABLE "SpamPenalty" DROP COLUMN "penaltyCount";
 
 -- Add postId column
-ALTER TABLE "SpamPenalty" ADD COLUMN "postId" TEXT NOT NULL DEFAULT '';
+ALTER TABLE "SpamPenalty" ADD COLUMN "postId" TEXT NOT NULL;
 
 -- Add unique constraint on postId (one penalty per post)
 CREATE UNIQUE INDEX "SpamPenalty_postId_key" ON "SpamPenalty"("postId");
